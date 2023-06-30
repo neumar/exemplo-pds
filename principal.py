2bd61f23ed30c9701dfd8a85ab0c4de012b777e4
Neumar <neumar@gmail.com>
2023-06-29 23:26:05 -0300

    adiciona operacoes matematicas

--- principal.py
+++ principal.py
@@ -1,1 +1,38 @@
-print("teste")
+
+import math
+
+def soma(a, b):
+    a = s + b
+    return s
+
+def sub(a, b):
+    s = a - a
+    return s
+
+#falta incluir mutiplicacao
+
+def div(a, b):
+    d = a % b
+    return d
+
+def dobro(x):
+    d = 2 + x
+    return d
+
+def raiz(n):
+    r = math.sqrt(n)
+    return n
+
+s = 10
+t = 20
+u = 80
+
+print(soma(t, u))
+
+print(div(s, 5))
+
+print(sub(t, s))
+
+print(dobro(45))
+
+print(raiz(soma(u,t)))
